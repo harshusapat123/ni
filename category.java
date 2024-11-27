5f0778258372718ab4c09e61786edd070b3f3fb0
harshusapat123 <104025063+harshusapat123@users.noreply.github.com>
2024-11-27 15:48:44 +0530

    Create category.java

---
+++ category.java
@@ -0,0 +1,16 @@
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products;
+
+    // Getters and Setters
+}
