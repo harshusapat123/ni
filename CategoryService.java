421c5d4a0c5f448b63eb55a9c2680acb4d0d8840
harshusapat123 <104025063+harshusapat123@users.noreply.github.com>
2024-11-27 15:52:01 +0530

    Create CategoryService.java

---
+++ CategoryService.java
@@ -0,0 +1,32 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Page<Category> getAllCategories(int page, int size) {
+        return categoryRepository.findAll(PageRequest.of(page, size));
+    }
+
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElse(null);
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category updateCategory(Long id, Category category) {
+        category.setId(id);
+        return categoryRepository.save(category);
+    }
+
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
